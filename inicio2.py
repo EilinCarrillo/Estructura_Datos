80bb606f141b06979343fef6a546e070f1ba9341
EilinCarrillo <eilincarrillo06@gmail.com>
2025-02-08 17:57:50 +0000

    inicios terminados

--- inicio2.py
+++ inicio2.py
@@ -1,11 +1,11 @@
-Temperatura=[]
+temperatura=[]
 sumatempertura=0
 
 for n in range (0,23):
-    T=int(input("Ingresa la temperatura"))
-    Temperatura.append(T)
-    sumatempertura=+T
-print (Temperatura)
+    t=int(input("Ingresa la temperatura"))
+    temperatura.append(t)
+    sumatempertura=+t
+print (temperatura)
 
 promedio=sumatempertura/23
 
@@ -13,7 +13,7 @@ print("El promedio de la temperatura es :", promedio )
 
 if promedio <20:
     print("Revisar la temperatura")
-elif 20<= promedio <=30:
+elif 20>= promedio <=30:
     print("La temperatura esta bien")
 else:
     print("Revisar la conducta de la temperatura")
