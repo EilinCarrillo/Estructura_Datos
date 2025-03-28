5152980c552a52c8fbe0f64c543d5b9807d04b32
EilinCarrillo <eilincarrillo06@gmail.com>
2025-03-28 23:42:00 +0000

    Terminado

--- practicaColas/model/ticket.py
+++ practicaColas/model/ticket.py
@@ -8,4 +8,4 @@ class Ticket(BaseModel):
     case_description: str # description of the case
     age: int # age of the person
     priority_attention: bool # priority attention? True or False
-    priority: Optional[int] = None  # Prioridad calculada por el sistema; no enviada por el cliente (puesto por friend de Esteban recomendado eliminar comentario..)
+    priority: Optional[int] = None  # Prioridad calculada por el sistema; no enviada por el cliente 
